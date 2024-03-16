49d7d3ece7d955d095d1a84fc585b1de7678a8b8
Chiragm550 <mchirag550@gmail.com>
2024-03-16 14:41:59 +0530

    Update the app

--- myapp/lib/widgets/home.dart
+++ myapp/lib/widgets/home.dart
@@ -12,13 +12,29 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final _formKey = GlobalKey<FormState>();
-  String fLength = '';
-  String fWidth = '';
-  String fSize = '';
-  String fConc = '';
+
+  final fieldNames = [
+    'accelerometer-x-l',
+    'accelerometer-y-l',
+    'accelerometer-z-l',
+    'Gyro-x-l',
+    'Gyro-y-l',
+    'Gyro-z-l',
+    'NC/SC-l'
+  ];
+
+  final fieldValues = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ];
 
   var data;
-  String output = '';
+  var output = "";
 
   Future<void> savingForm() async {
     final validation = _formKey.currentState!.validate();
@@ -41,50 +57,74 @@ class _HomeState extends State<Home> {
           margin: const EdgeInsets.all(10),
           child: Form(
               key: _formKey,
-              child: Column(children: [
-                TextFormField(
-                  decoration: const InputDecoration(labelText: "Enter fLength"),
-                  onSaved: (newValue) {
-                    fLength = newValue.toString();
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: "Enter fWidth"),
-                  onSaved: (newValue) {
-                    fWidth = newValue.toString();
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: "Enter fSize"),
-                  onSaved: (newValue) {
-                    fSize = newValue.toString();
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: "Enter fConc"),
-                  onSaved: (newValue) {
-                    fConc = newValue.toString();
-                  },
-                ),
+              child: ListView(scrollDirection: Axis.vertical, children: [
+                ...List.generate(fieldNames.length, (index) {
+                  return TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "Enter ${fieldNames[index]}"),
+                    onSaved: (newValue) {
+                      fieldValues[index] = newValue.toString();
+                    },
+                  );
+                }),
                 TextButton(
                     onPressed: () async {
                       savingForm();
-
                       const url = 'http://10.0.2.2:5000/predict';
                       data = await http.post(Uri.parse(url),
                           body: json.encode({
-                            'fLength': fLength,
-                            'fWidth': fWidth,
-                            'fSize': fSize,
-                            'fConc': fConc
+                            fieldNames[0]: fieldValues[0],
+                            fieldNames[1]: fieldValues[1],
+                            fieldNames[2]: fieldValues[2],
+                            fieldNames[3]: fieldValues[3],
+                            fieldNames[4]: fieldValues[4],
+                            fieldNames[5]: fieldValues[5],
+                            fieldNames[6]: fieldValues[6],
                           }));
 
+                      for (int i = 0; i < 7; i++) {
+                        print('${fieldValues[i]} ');
+                      }
                       print('Data is ${json.decode(data.body)}');
+
+                      final decoded = json.decode(data.body);
+
+                      setState(() {
+                        output = decoded['result'] == 1
+                            ? 'Parkinsons'
+                            : 'No Parkinsons';
+                      });
                     },
-                    child: const Text('Submit')),
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(10.0),
+                      backgroundColor: Colors.blue,
+                    ),
+                    child: const Text(
+                      'Submit',
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          ),
+                    )),
+                Center(
+                    child: Text(
+                  output.toString(),
+                  style: const TextStyle(
+                      color: Colors.red,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold),
+                ))
               ])),
         ),
       ),
     );
   }
 }
+
+// TextFormField(
+//                   decoration:
+//                       const InputDecoration(labelText: "Enter accelometer-x-l"),
+//                   onSaved: (newValue) {
+//                     fLength = newValue.toString();
+//                   },
+//                 )
